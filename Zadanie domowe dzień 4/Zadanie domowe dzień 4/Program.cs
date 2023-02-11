23db29ab5e95a1f193362e3221e13e2eafd6e559
uibsed <uibsed@gmail.com>
2023-02-11 13:36:24 +0100

    Żryj gówno

---
+++ Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
@@ -0,0 +1,29 @@
+﻿var name = "Barnaba";
+var gender = "man";
+var age = 50;
+if (name == "Barnaba" && age == 50)
+{
+    Console.WriteLine("Barnaba lat 50");
+}
+else if (gender == "man")
+{
+    if (age < 50)
+    {
+        Console.WriteLine("Mężczyzna poniżej 50 lat");
+    }
+    if (age > 50)
+    {
+        Console.WriteLine("Mężczyzna po 50 roku życia");
+    }
+}
+else if (gender == "woman")
+{
+    if (age < 50)
+    {
+        Console.WriteLine("Kobieta poniżej 50 lat");
+    }
+    if (age > 50)
+    {
+        Console.WriteLine("Kobieta po 50 roku życia");
+    }
+}
